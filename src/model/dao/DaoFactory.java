c3fcbd873a5f0d11d60add315ecafeb3d5d3d515
Lucas <lucas_dev@outlook.com.br>
2024-03-05 14:36:41 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 import model.entites.Seller;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
